4a96b7d4c099bc19ac6ef0635e6a6dd56210f06a
Liangqi He <115479065+DawsonNat@users.noreply.github.com>
2024-12-07 18:00:11 +0800

    添加s3存储桶设置，上传图片功能

--- chat_service/config.js
+++ chat_service/config.js
@@ -8,6 +8,21 @@ module.exports = {
         database: "Realtime_chat"
     },
 
+    // s3对象存储配置 https://www.lingshulian.com 超低成本的对象存储平台
+      s3Config: {
+        config: {
+          credentials: {
+            accessKeyId: 'dadef56f24f7241ad007309289d7a681', // secretId
+            secretAccessKey: 'b3a5194428f697fb2e6c0db36d38a221a2edc3c403ea400ac755e1e44244ca2a' // secretKey
+          },
+          endpoint: 'https://s3-us-east-1.ossfiles.com', // https:// + 获取的的端点
+          region: 'us-east-1'
+        },
+        // 上传目录 可自定义
+        path: 'chat',
+        // 上传桶
+        bucket: "realtime" // 存储桶
+      },
 
     jwtConfig: {
         secret: 'Green_Turtle',
